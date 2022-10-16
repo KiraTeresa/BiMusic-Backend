169822d2824e2f9a3e54970ef809b0ee2825016d
Kira <kira.soba@gmx.de>
2022-10-16 14:44:51 +0200

    Add: Comment Model

---
+++ models/Comment.model.js
@@ -0,0 +1,26 @@
+const {Schema, model} = require('mongoose');
+const User = require('./User.model');
+
+const commentSchema = new Schema(
+    {
+        text: {
+            type: String,
+            required: true,
+            max: 100
+        },
+        author: {
+            type: Schema.Types.ObjectId,
+            ref: User,
+            required: true
+        },
+        project: {
+            type: Schema.Types.ObjectId,
+            ref: Project,
+            required: true
+        }
+    }
+)
+
+const Comment = model('Comment', commentSchema)
+
+module.exports = Comment;
