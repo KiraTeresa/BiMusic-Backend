874b9ce5aead1fcd1fb9d6057f605c2d0d0efc11
Nico Schmock <nicoschmock@web.de>
2022-10-18 18:52:36 +0200

    profile router

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,11 +6,4 @@ router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
-router.post("/profile",async (req, res, next) => {
-  try{const email = req.body.email
-  const userInfo = await User.findOne({email:email})
-  res.json(userInfo);}
-  catch(err){console.log(err)}
-});
-
 module.exports = router;
---
+++ routes/profile.routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User.model.js")
+
+router.post("/",async (req, res, next) => {
+    try{const email = req.body.email
+    const userInfo = await User.findOne({email:email},"-password") //Exclude password from req
+  
+    res.json(userInfo);}
+    catch(err){console.log(err)}
+  });
+  
+  module.exports = router;
