5919471e5aa546994a1b213f1b9e0205bc89b55f
Kira <kira.soba@gmx.de>
2022-10-21 11:40:17 +0200

    Feat: creating sample without project --> done // OPEN TO DO: uploading sample to cloudinary and storing that link instead

--- routes/samples.routes.js
+++ routes/samples.routes.js
@@ -7,7 +7,7 @@ const Sample = require('../models/Sample.model');
 router.get("/create", async (req, res) => {
     console.log("REQ SAMPLE CREATE: ", req.query)
     const {projectId} = req.query
-    if(req.query){
+    if(projectId){
         await Project.findById(projectId).populate('initiator').then((project)=> res.json(project)).catch(err => console.log(err))}
 
     else {
@@ -17,7 +17,7 @@ router.get("/create", async (req, res) => {
 
 router.post("/create", async (req, res) => {
     console.log("BODY: ", req.body)
-    const {form, addedToProject} = req.body
+    const {form, projectId} = req.body
     const user = mongoose.Types.ObjectId(form.artist);
 
     // TO DO: Validation for valid link
@@ -26,13 +26,14 @@ router.post("/create", async (req, res) => {
     await Sample.create({...form, artist: user}).then(async(newSample)  => {
          console.log("Created new sample in db: ", newSample)
          
-             if(addedToProject){
-                 await Project.findByIdAndUpdate(addedToProject, {sample: newSample}, {new: true}).then((updatedProject) => {
+             if(projectId){
+                 await Project.findByIdAndUpdate(projectId, {sample: newSample}, {new: true}).then((updatedProject) => {
                     console.log("Sample was added to Project: ", updatedProject)
                  })
              }
 
              console.log("Successfully added a sample")
+             res.json("Backend done with adding new sample.")
     }).catch((err) => console.log("Something went wrong when adding a new sample.", err))
 })
 
