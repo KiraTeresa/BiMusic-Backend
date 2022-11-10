458fe634da0d91889df7c57cf742deccbe7b8ed3
Kira <kira.soba@gmx.de>
2022-11-10 08:20:58 +0100

    Fix: missing keys

--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -10,14 +10,16 @@ router.post("/:projectId", isLoggedIn, async (req, res) => {
     const {text} = req.body.form;
 
     if(!text){
-        res.status(400).json({message: "Forgot to type the actual comment?"})
+        return res.status(400).json({message: "Forgot to type the actual comment?"})
     }
 
     // create the comment:
     await Comment.create({text, author: req.user, project: Types.ObjectId(projectId)}).then(async (result) => {
 
         // add comment to project:
-        await Project.findByIdAndUpdate(projectId, {"$push": {comments: result}}, {"new": true}).then(() => res.json("Comment successfully added."))
+        await Project.findByIdAndUpdate(projectId, {"$push": {comments: result}}, {"new": true}).then(() => {
+            return res.json("Comment successfully added.")
+        })
     }).catch((err) => res.status(500).json({message: "Please try again", err}))
 })
 
